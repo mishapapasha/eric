9f1c77cbd23bf18bda9f332473bfc34bc37c07e4
mishapapasha <mishapapasha@gmail.com>
2018-05-22 14:34:40 +0300

    locgic done

--- js/currencyInfoService.js
+++ js/currencyInfoService.js
@@ -1,18 +1,47 @@
 var baseCurrency = 'USD';
 
-var currencyAPI = {};
+//ci = currency information
+var ciService = {};
 
 var currencies = {
-    'USD':  1,
-    'EUR':  0.844798,
-    'GBP':  0.740774,
-    'ILS':  3.60177,
-    'CAD':  1.28549,
-    'AUD':  1.33617,
-    'INR':  67.8283,
-    'SGD':  1.34248
+    'USD':  {
+        'currency': 1,
+        'sign':     ''
+    },
+    'EUR':  {
+        'currency': 0.844798,
+        'sign':     ''
+    },
+    'GBP':  {
+        'currency': 0.740774,
+        'sign':     ''
+    },
+    'ILS':  {
+        'currency': 3.60177,
+        'sign':     '&#8362;'
+    },
+    'CAD':  {
+        'currency': 1.28549,
+        'sign':     ''
+    },
+    'AUD':  {
+        'currency': 1.33617,
+        'sign':     ''
+    },
+    'INR':  {
+        'currency': 67.8283,
+        'sign':     ''
+    },
+    'SGD':  {
+        'currency': 1.34248,
+        'sign':     ''
+    }
 };
 
-currencyAPI.getAvailableCurrencies = function() {
+ciService.getAvailableCurrencies = function() {
     return Object.keys(currencies);
+}
+0
+ciService.getCurrencyByKey = function(key) {
+    return currencies[key];
 }
--- js/initService.js
+++ js/initService.js
@@ -6,7 +6,9 @@ fromCurrency,
 dropDownFrom, 
 arrow,
 toCurrency, 
-dropDownTo;
+dropDownTo,
+resultBtn,
+resultSpan;
 
 function createConvertorHeader(header) {
     //create the header element
@@ -37,6 +39,7 @@ function createaAmountContainer(container) {
     amount = document.createElement('input');
     amount.type = "text";
     amount.id = "amount";
+    amount.placeholder = "default 1";
     //append the input element to the container
     container.appendChild(amount);
     return container;
@@ -44,11 +47,11 @@ function createaAmountContainer(container) {
 
 function createFromContainer(container) {
     //create the from dropdown container
-    var container = this.document.createElement('div');
+    var container = document.createElement('div');
     container.classList.add('dropdownContainer');
     //create list of the currencies for dropdown
-    dropDownFrom = this.document.createElement('ul');
-    currencyAPI.getAvailableCurrencies().forEach(function(currency){
+    dropDownFrom = document.createElement('ul');
+    ciService.getAvailableCurrencies().forEach(function(currency){
         let cureencyElement = document.createElement('li');
         cureencyElement.addEventListener("click", function(){
             fromCurrency.innerHTML = currency;
@@ -62,10 +65,13 @@ function createFromContainer(container) {
     //create button for from dropdown
     fromCurrency = document.createElement('button');
     fromCurrency.addEventListener("click", function(){
+        if(dropDownTo.classList.contains('show')){
+            dropDownTo.classList.toggle('show');
+        }
         dropDownFrom.classList.toggle("show");
     });
     fromCurrency.classList.add('dropBtn');
-    fromCurrency.innerHTML = "FROM";
+    fromCurrency.innerHTML = "USD";
     //append the button and dropdown to container
     container.appendChild(fromCurrency);
     container.appendChild(dropDownFrom);
@@ -82,11 +88,11 @@ function createArrow(arrow) {
 
 function createToContainer(container) {
     //create the to container
-    container = this.document.createElement('div');
+    container = document.createElement('div');
     container.classList.add('dropdownContainer');
     //create list of currencies for to dropdown
-    dropDownTo = this.document.createElement('ul');
-    currencyAPI.getAvailableCurrencies().forEach(function(currency){
+    dropDownTo = document.createElement('ul');
+    ciService.getAvailableCurrencies().forEach(function(currency){
         let cureencyElement = document.createElement('li');
         cureencyElement.addEventListener("click", function(){
             toCurrency.innerHTML = currency;
@@ -100,16 +106,51 @@ function createToContainer(container) {
     //create button for to dropdown
     toCurrency = document.createElement('button');
     toCurrency.addEventListener("click", function(){
+        if(dropDownFrom.classList.contains('show')){
+            dropDownFrom.classList.toggle('show');
+        }
         dropDownTo.classList.toggle("show");
     });
     toCurrency.classList.add('dropBtn');
-    toCurrency.innerHTML = "TO";
+    toCurrency.innerHTML = "EUR";
     //append the button and the dropdown list to container
     container.appendChild(toCurrency);
     container.appendChild(dropDownTo);
     return container;
 }
 
+function createResultContainer(container) {
+    container = document.createElement('div');
+    container.id = 'resultContainer';
+
+    resultBtn = document.createElement('button');
+    resultBtn.id = 'resultBtn';
+    resultBtn.innerHTML = 'CONVERT';
+    resultBtn.addEventListener("click", convertAmount);
+
+    resultSpan = document.createElement('span');
+    resultSpan.id = 'resultSpan';
+    resultSpan.innerHTML = "RESULT";
+
+    container.appendChild(resultBtn);
+    container.appendChild(resultSpan);
+    return container;
+}
+
+function convertAmount() {
+    if(!parseFloat(amount.value) || isNaN(amount.value)){
+        console.log("Amount is not a number");
+        return;
+    }
+    //console.log(amount.value);
+    var activeFromCurrency = ciService.getCurrencyByKey(fromCurrency.innerHTML);
+    var activeToCurrency = ciService.getCurrencyByKey(toCurrency.innerHTML);
+    // console.log(activeFromCurrency);
+    // console.log(activeToCurrency);
+    var result = ciService.getCurrencyByKey(baseCurrency).currency;
+    result = result * (amount.value / activeFromCurrency.currency) * activeToCurrency.currency;
+    resultSpan.innerHTML = result.toFixed(4) + activeToCurrency.sign;
+}
 
 window.onload = function() {
     var main = document.getElementById('mainContainer');
@@ -132,6 +173,9 @@ window.onload = function() {
     //create the to currency dropdown
     var toContainer = createToContainer(toContainer);
 
+    //create result section
+    var resultContainer = createResultContainer(resultContainer);
+
     //Append element to the document
     //Main
     main.appendChild(header);
@@ -141,4 +185,5 @@ window.onload = function() {
     convertor.appendChild(fromContainer);
     convertor.appendChild(arrow);
     convertor.appendChild(toContainer);
+    convertor.appendChild(resultContainer);
 }
